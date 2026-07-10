307885fc8a7eb34a6a1e692c63812b6d3ea98ba7
hastidhameliya <hastidhameliya16@gmail.com>
2026-07-10 13:11:38 +0530

    api responces

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ const app = express();
 dotenv.config({ path: "./.env" });
 
 connectDB()
+.then(() =>{ 
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("Error connecting to MongoDB:", err);
+})
+
+
 
 
 
